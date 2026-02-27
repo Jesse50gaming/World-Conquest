9592b32100c78d43b9c07887c3bffeccc1a6cfc6
Jesse50gaming <128247303+Jesse50gaming@users.noreply.github.com>
2026-02-26 19:31:51 -0600

    l

--- src/main/java/com/worldconquest/technology/TechTree.java
+++ src/main/java/com/worldconquest/technology/TechTree.java
@@ -13,61 +13,17 @@ public class TechTree {
 
     public TechTree(ArrayList<Tech> techs) {
         this.techs = techs;
-        updatePosition();
+        
     }
 
+   
     public void addTech(Tech tech) {
         techs.add(tech);
-        updatePosition();
+        
     }
-
     public void addTechs(ArrayList<Tech> techs) {
         this.techs.addAll(techs);
-    }
-
-    private void updatePosition() {
         
-        ArrayList<Tech> locationNotSet = new ArrayList<>();
-        locationNotSet.addAll(techs);
-        Tech rootTech = null;
-        HashMap<Tech, Integer> techDepth = new HashMap<>();
-
-        
-        //find all depths and root
-        for (Tech tech : locationNotSet) {
-            int depth = findDepth(tech);
-            techDepth.put(tech, depth);
-            if (depth == 0) {
-                rootTech = tech;
-                rootTech.setCoord(new Vector2f(0,0));
-            }
-            
-        }
-        
-        //set in relevance to root
-
-        for (Tech tech : techs) {
-
-        }
-
     }
-    //TODO most likely an error
-    private int findDepth(Tech tech) {
-        int depth = -1;
-        for (Tech preReq : tech.getPreReqs().keySet()) {
-
-            depth = Math.max(depth, 1 + findParentDepth(preReq));
-        }
-
-        return depth;
-
-    }
-    
-    private int findParentDepth(Tech tech) {
-        if (tech.getPreReqs().isEmpty() || tech.getPreReqs() == null) {
-            return 0;
-        }
-        
-        return findDepth(tech);
-    }
+   
 }
